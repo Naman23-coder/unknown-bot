bc560a723b468cb5810f62d884f8128798e3e5e5
Naman Sharma <namansharma232009@gmail.com>
2022-06-21 10:15:07 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+
+class Bot(commands.Bot):
+  def __init__(self) -> None:
+    pass
+  
+  async def setup_hook(self) -> None:
+    async for filename in os.listdir('./cogs'):
+      if filename.endswith('.py'):
+          await bot.load_extension(f'cogs.{filename[:-3]}')
+ 
