8ae2c005f7aaa2a6ad7fc90eed51cb79b8de3b3d
Naman <namansharma232009@gmail.com>
2022-06-22 11:07:06 +0530

    changed db to mongodb

--- db/dbmanager.py
+++ db/dbmanager.py
@@ -1,27 +1,30 @@
-import asyncpg
+from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 db_ = os.environ.get("db")
 
+db = AsyncIOMotorClient(db_)["db"]
+
 
 async def create_user(user_id: int):
-    db = await asyncpg.connect(db_)
-    await db.conn.execute(f"INSERT INTO user (id,balance) VALUES ({user_id}, 0)")
-    await db.commit()
+    async with await db.start_session() as s:
+        query = {"_id": user_id, "pocket": 0, "bank": 0}
+        await db.insert_one(query, session=s)
 
 
 async def user_exist(user_id: int):
-    db = await asyncpg.connect(db_)
-    return await db.fetch(
-        f"""
-        select exists(select 1 from user where id={user_id})
-        """
-    )
+    async with await db.start_session() as s:
+        return (
+            await db.collection.count_documents({"_id": user_id}, limit=1, session=s)
+            != 0
+        )
 
 
 async def get_balance(user_id: int):
-    pass
+    async with await db.start_session() as s:
+        return await db.find_one({"_id": user_id}, session=s)["pocket"]
 
 
 async def get_bank_balance(user_id: int):
-    pass
+    async with await db.start_session() as s:
+        return await s.find_one({"_id": user_id})["bank"]
